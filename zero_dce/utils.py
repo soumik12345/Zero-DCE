5471c2027b156a1f5d46f46adf20ed6d96be4789
soumik12345 <19soumik.rakshit96@gmail.com>
2020-11-14 23:47:03 +0530

    Added Garbage collection utils

--- zero_dce/utils.py
+++ zero_dce/utils.py
@@ -1,4 +1,6 @@
 import os
+import gc
+import torch
 import wandb
 import gdown
 import subprocess
@@ -29,3 +31,36 @@ def plot_result(image, enhanced):
     fig.add_subplot(1, 2, 2).set_title('Enhanced Image')
     _ = plt.imshow(enhanced)
     plt.show()
+
+
+def pretty_size(size):
+    """Pretty prints a torch.Size object"""
+    assert (isinstance(size, torch.Size))
+    return " × ".join(map(str, size))
+
+
+def dump_tensors(gpu_only=True):
+    """Prints a list of the Tensors being tracked by the garbage collector."""
+    total_size = 0
+    for obj in gc.get_objects():
+        try:
+            if torch.is_tensor(obj):
+                if not gpu_only or obj.is_cuda:
+                    print("%s:%s%s %s" % (type(obj).__name__,
+                                          " GPU" if obj.is_cuda else "",
+                                          " pinned" if obj.is_pinned else "",
+                                          pretty_size(obj.size())))
+                    total_size += obj.numel()
+            elif hasattr(obj, "data") and torch.is_tensor(obj.data):
+                if not gpu_only or obj.is_cuda:
+                    print("%s → %s:%s%s%s%s %s" % (type(obj).__name__,
+                                                   type(obj.data).__name__,
+                                                   " GPU" if obj.is_cuda else "",
+                                                   " pinned" if obj.data.is_pinned else "",
+                                                   " grad" if obj.requires_grad else "",
+                                                   " volatile" if obj.volatile else "",
+                                                   pretty_size(obj.data.size())))
+                    total_size += obj.data.numel()
+        except Exception as e:
+            pass
+    print("Total size:", total_size)
